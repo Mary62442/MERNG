ccf0d883043470e2c92af13e1e6e4ff28bc3457e
Mary62442 <maria.bu62442@gmail.com>
2019-12-02 15:36:18 +0100

    set up server and db connection

--- index.js
+++ index.js
@@ -2,15 +2,28 @@ const {ApolloServer} = require("apollo-server");
 const mongoose = require("mongoose");
 const {MONGODB} = require("./config.js");
 const gql = require("graphql-tag");
+const Post = require("./models/Post");
 const typeDefs = gql`
+type Post {
+    id:ID!,
+    body:String!,
+    createdAt: String!,
+    username: String!
+}
     type Query {
-        sayHi:String!
+        getPosts:[Post]
     }
 `;
-
 const resolvers = {
     Query: {
-        sayHi: () => "Hello World!!!!!!!!"
+        async getPosts() {
+            try{
+                const posts = await Post.find();
+                return posts;
+            }catch(err) {
+                throw new Error(err);
+            }
+        }
     }
 }
 
