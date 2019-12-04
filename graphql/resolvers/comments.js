2714b010eda4f14df6db2b671aa5d859cc45b842
Mary62442 <maria.bu62442@gmail.com>
2019-12-04 14:53:08 +0100

    Created likes and comments mutation, post subscription and likes and comments count

---
+++ graphql/resolvers/comments.js
@@ -0,0 +1,43 @@
+const Post = require("../../models/Post");
+const {UserInputError, AuthenticationError} = require("apollo-server");
+const checkAuth = require("../../util/checkauth");
+
+module.exports = {
+    Mutation: {
+        createComment : async(_,{postId,body},context) => {
+            const user = checkAuth(context);
+            if(body.trim() === "") {
+                throw new UserInputError("Comment must not be empty", {
+                    errors: {
+                        body:"Comment body must not be empty"
+                    }
+                })
+            }
+            const post = await Post.findById(postId);
+            if (post) {
+                post.comments.unshift({
+                    body,
+                    username: user.username,
+                    createdAt: new Date().toISOString()
+                });
+                await post.save();
+                return post;
+            } else throw new UserInputError("Post not found");            
+        },
+
+        deleteComment: async(_,{ postId, commentId },context) => {
+            const {username} = checkAuth(context);
+            const post = await Post.findById(postId);
+            if (post) {
+                const commentIndex = post.comments.findIndex(c => c.id === commentId);
+                if(post.comments[commentIndex].username === username) {
+                    post.comments.splice(commentIndex, 1);
+                    await post.save();
+                    return post;
+                } else {
+                    throw new AuthenticationError("Action not allowed")
+                }                
+            } else throw new UserInputError("Post not found")
+        }
+    }
+}
--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-const {ApolloServer} = require("apollo-server");
+const {ApolloServer, PubSub} = require("apollo-server");
 const mongoose = require("mongoose");
 const {MONGODB} = require("./config.js");
 const typeDefs = require("./graphql/typedefs");
 const resolvers = require("./graphql/resolvers");
 
+const pubSub = new PubSub();
 
 const server = new ApolloServer({
     typeDefs,
     resolvers,
-    context: ({req}) => ({req})
+    context: ({req}) => ({req, pubSub})
 })
 
 
