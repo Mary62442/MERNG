eb194cd5651667f1a972759b3fee68dd411484af
Mary62442 <maria.bu62442@gmail.com>
2019-12-04 11:15:48 +0100

    created mutations getPost deletePost and createPost

--- graphql/typedefs.js
+++ graphql/typedefs.js
@@ -1,29 +1,32 @@
 const {gql} = require("apollo-server");
 module.exports = gql`
     type Post {
-        id:ID!,
-        body:String!,
-        createdAt: String!,
+        id:ID!
+        body:String!
+        createdAt: String!
         username: String!
     }
     type User {
-        id:ID!,
-        email:String!,
-        username:String!,
-        token:String!,
+        id:ID!
+        email:String!
+        username:String!
+        token:String!
         createdAt:String!
     }
     input RegisterInput {
-        username:String!,
-        password:String!,
-        confirmPassword:String!,
+        username:String!
+        password:String!
+        confirmPassword:String!
         email:String!
     }
     type Query {
         getPosts:[Post]
+        getPost(postId: ID!): Post
     }
     type Mutation {
-        register(registerInput : RegisterInput):User!,
+        register(registerInput: RegisterInput):User!
         login(username:String!, password:String!):User!
+        createPost(body:String!):Post!
+        deletePost(postId:ID!):String!
     }
 `;
